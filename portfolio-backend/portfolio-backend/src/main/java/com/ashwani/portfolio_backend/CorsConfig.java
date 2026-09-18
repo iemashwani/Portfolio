5223818a951ac38b251fc7633baccaad9a56f469
AshwaniSingh12 <as3141538@gmail.com>
2026-09-18 14:06:05 +0530

    Fix Cors Config issue

--- portfolio-backend/portfolio-backend/src/main/java/com/ashwani/portfolio_backend/CorsConfig.java
+++ portfolio-backend/portfolio-backend/src/main/java/com/ashwani/portfolio_backend/CorsConfig.java
@@ -9,16 +9,13 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://127.0.0.1:5500",
-                        "http://localhost:5500"
+                        "http://localhost:5500",
+                        "https://iemashwani.github.io"
                 )
-                .allowedMethods(
-                        "GET",
-                        "POST",
-                        "OPTIONS"
-                );
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
